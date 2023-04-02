93685cf82cc0de8acff14a6b1832800ed5dccfbd
Chino Franco <chino.franco@gmail.com>
2023-04-02 17:44:28 +0900

    Added base recommendation model

--- src/base.py
+++ src/base.py
@@ -1,17 +1,16 @@
-"""
-project_template base module.
+import os
+import requests
+import spotipy
 
-This is the principal module of the project_template project.
-here you put your main classes and objects.
 
-Be creative! do whatever you want!
-
-If you want to replace this with a Flask application run:
-
-    $ make init
-
-and then choose `flask` as template.
-"""
-
-# example constant variable
-NAME = "project_template"
+class SongRecommender:
+    def __init__(self) -> None:
+        self.__entrypoint = "https://accounts.spotify.com/api/token"
+        self.__credentials = {
+            "grant_type": "client_credentials", 
+            "client_id": os.environ["CLIENT_ID"], 
+            "client_secret": os.environ["CLIENT_SECRET"]
+        }
+        authentication = requests.post(self.__entrypoint, self.__credentials).json()["access_token"]
+        self.api_client = spotipy.client.Spotify(auth=authentication)
+    
